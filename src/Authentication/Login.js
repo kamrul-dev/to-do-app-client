e39ef9762129a24ee4799aa5dd3851857fd15824
Kamrul Hasan <kamrulsdev@gmail.com>
2022-05-19 04:14:44 +0600

    Google sign in Completed

--- src/Authentication/Login.js
+++ src/Authentication/Login.js
@@ -1,13 +1,31 @@
 import React from 'react';
-
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from '../firebase.init';
+import Loading from '../Shared/Loading';
+import { useNavigate } from 'react-router-dom';
 const Login = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const navigate = useNavigate();
+    if (loading) {
+        return <Loading></Loading>
+    }
+
+    let errorShow;
+    if (error) {
+        errorShow = <p>{error?.message}</p>
+    }
+
+    if (user) {
+        navigate('/appMain');
+    }
     return (
         <div className='flex justify-center items-center h-screen'>
             <div className="card  w-96 bg-base-100 shadow-xl">
                 <h2 className='text-xl text-center'>Welcome to "To Do App"</h2>
                 <div className="card-body items-center text-center">
-                    <input type="submit" value="Sign in with Google" className="input input-bordered input-primary w-full max-w-xs" />
+                    <input onClick={() => signInWithGoogle()} type="submit" value="Sign in with Google" className="input input-bordered input-primary w-full max-w-xs" />
                 </div>
+                {errorShow}
             </div>
         </div>
     );

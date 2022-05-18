a7fd6275fd0b37991c15811873d38633ebdd0442
Kamrul Hasan <kamrulsdev@gmail.com>
2022-05-19 00:01:26 +0600

    Intalled daisyUI

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div>
       <h2 className='text-3xl text-green-500'>Hello to do app</h2>
+      <button class="btn btn-secondary">Button</button>
     </div>
   );
 }

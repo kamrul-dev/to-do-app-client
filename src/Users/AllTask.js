be58c665c3bf2db7bd8044158a8a6869a2c6e7f6
Kamrul Hasan <kamrulsdev@gmail.com>
2022-05-19 03:48:09 +0600

    Task Deletion part completed

--- src/Users/AllTask.js
+++ src/Users/AllTask.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
 import Loading from '../Shared/Loading';
 import Task from './Task';
 
@@ -9,6 +10,20 @@ const AllTask = () => {
             .then(res => res.json())
             .then(data => setTasks(data))
     }, []);
+
+    const handleTaskDelete = (id) => {
+        const url = `http://localhost:5000/task/${id}`;
+        fetch(url, {
+            method: 'DELETE'
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                const remaining = tasks.filter(task => task._id !== id);
+                setTasks(remaining);
+                toast.success('Sucessfully Deleted !')
+            })
+    }
 
     if (!tasks.length > 0) {
         return <Loading></Loading>
@@ -21,6 +36,7 @@ const AllTask = () => {
                     tasks.map(task => <Task
                         key={task._id}
                         task={task}
+                        handleTaskDelete={handleTaskDelete}
                     ></Task>)
                 }
             </div>
--- src/Users/Task.js
+++ src/Users/Task.js
@@ -1,16 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Task = ({ task }) => {
-    const { taskName, description } = task;
+const Task = ({ task, handleTaskDelete }) => {
+    const {_id, taskName, description } = task;
     return (
         <div>
             <div className="card lg:max-w-lg bg-base-100 shadow-xl">
                 <div className="card-body">
                     <h2 className="card-title">{taskName}</h2>
+                    <p>{_id}</p>
                     <p>{description}</p>
                     <div className="card-actions justify-center mt-4">
                         <button className="btn btn-primary">Complete</button>
-                        <button className="btn btn-error">Delete</button>
+                        <button onClick={()=> handleTaskDelete(_id)} className="btn btn-error">Delete</button>
                     </div>
                 </div>
             </div>
